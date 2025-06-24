5cfe2e07d673c1acc42febddd7a486b5bafcaa96
Dissa2003 <164222210+Dissa2003@users.noreply.github.com>
2025-06-25 02:21:16 +0530

    add movie posters

--- app.py
+++ app.py
@@ -24,27 +24,29 @@ def recommand(movie):
     index=movies[movies['title']==movie].index[0]
     distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
     recommand_movie=[]
+    recommand_poster=[]
 
     for i in distance[0:5]:
           recommand_movie.append(movies.iloc[i[0]].title)
+          recommand_poster.append(fetch_poster(movies.iloc[i[0]].id))
 
-    return recommand_movie
+    return recommand_movie,recommand_poster
 
 if st.button("Show Recommend"):
-    movie_name = recommand(selectvalue)
+    movie_name, movie_poster = recommand(selectvalue)
     col1,col2,col3,col4,col5=st.columns(5)
     with col1:
         st.text(movie_name[0])
-       
+        st.image(movie_poster[0])
     with col2:
         st.text(movie_name[1])
-       
+        st.image(movie_poster[1])
     with col3:
         st.text(movie_name[2])
-        
+        st.image(movie_poster[2])
     with col4:
         st.text(movie_name[3])
-        
+        st.image(movie_poster[3])
     with col5:
         st.text(movie_name[4])
-       
+        st.image(movie_poster[4])
